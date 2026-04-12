da8ed949d35269b94d6efc1a627eac1b2b7da5c6
ngaoalex01-dev <ngaoalex01@gmail.com>
2026-04-12 15:56:31 +0300

    change in CheckAuth in authStore

--- frontend/src/store/authStore.js
+++ frontend/src/store/authStore.js
@@ -65,7 +65,9 @@ export const  useAuthStore = create((set) => ({
   checkAuth: async () => {
     	set({ isCheckingAuth: true, error: null });
 		try {
-			const response = await axios.get(`${API_URL}/check-auth`);
+			const response = await axios.get(`${API_URL}/check-auth`, {
+        withCredentials: true,
+      });
 			set({ user: response.data.user, isAuthenticated: true, isCheckingAuth: false });
 		} catch (error) {
 			set({ error: null, isCheckingAuth: false, isAuthenticated: false });
